03ef03a6a4a9ba0d9106ade9bd9197e58b5591a5
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2021-04-10 18:56:56 -0400

    Remove Build From Routes

--- routes/app.js
+++ routes/app.js
@@ -4,7 +4,7 @@ let path = require('path');
 
 /* GET home page. */
 router.get(['/', '/*'], function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 module.exports = router;
