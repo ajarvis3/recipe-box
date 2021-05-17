f9ebdbec8550126234784978d3c877aff3505724
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2021-05-16 23:39:52 -0400

    Connect Edit to Backend

--- src/dashboard/components/add-recipe/RecipeFields.tsx
+++ src/dashboard/components/add-recipe/RecipeFields.tsx
@@ -4,14 +4,16 @@ import Grid from "@material-ui/core/Grid";
 import { Dispatch } from "react";
 import { SetStateAction } from "react";
 import currentRecipeIndexState from "../../recoil/CurrentRecipeIndex";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import userRecipesState from "../../../recoil/UserRecipes";
 import RecipeFieldInput from "./RecipeFieldInput";
 import { Button, makeStyles } from "@material-ui/core";
+import authenticatedFetch from "../../../account/fetch/authenticatedfetch";
+import IRecipeData from "../../types/RecipeData";
 
 const RecipeFields: FunctionComponent = () => {
    const recipeIndex = useRecoilValue(currentRecipeIndexState);
-   const recipes = useRecoilValue(userRecipesState);
+   const [recipes, setRecipes] = useRecoilState(userRecipesState);
    const currentRecipe = recipes[recipeIndex];
    const [title, setTitle] = useState(currentRecipe.title);
    const [author, setAuthor] = useState(currentRecipe.author);
@@ -26,8 +28,37 @@ const RecipeFields: FunctionComponent = () => {
       };
    };
 
+   const getNewRecipe = () => {
+      return {
+         ...currentRecipe,
+         title: title,
+         author: author,
+         source: source,
+         description: description,
+         imageUrl: imageUrl,
+         comments: comments,
+      };
+   };
+
+   const onSubmit = (event: React.SyntheticEvent) => {
+      event.preventDefault();
+      const newRecipe = getNewRecipe();
+      const newRecipes = recipes.slice();
+      console.log(newRecipe);
+      // deal with backend things...
+      authenticatedFetch(
+         `/content/recipes?id=${newRecipe._id}`,
+         JSON.stringify({ recipe: newRecipe }),
+         "PATCH"
+      ).then((value: IRecipeData) => {
+         console.log(value);
+         newRecipes[recipeIndex] = value;
+         setRecipes(newRecipes);
+      });
+   };
+
    return (
-      <Box component="form">
+      <Box component="form" onSubmit={onSubmit}>
          <Grid container direction="column" spacing={0}>
             <RecipeFieldInput
                fieldName="Title"
