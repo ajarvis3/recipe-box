bc4668f168eb83810ec5e82055bc3c8e538ba960
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2023-03-18 18:23:04 -0400

    Finishing OAuth login on client side

--- src/account/SignInGoogle.tsx
+++ src/account/SignInGoogle.tsx
@@ -1,32 +1,46 @@
 import { GoogleLogin } from "@react-oauth/google";
+import { Navigate } from "react-router";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import loginState from "../recoil/LoginState";
+import userIdState from "../recoil/UserId";
 import generalFetch from "./fetch/GeneralFetch";
+import setToken from "./utils/settoken";
 
-const SignInGoogle = () => (
-   <GoogleLogin
-      nonce=""
-      onSuccess={(credentialResponse) => {
-         generalFetch(
-            "auth/oauth",
-            JSON.stringify({
-               credential: credentialResponse.credential,
-               clientId: credentialResponse.clientId,
-            }),
-            {
-               "Content-type": "application/json; charset=UTF-8",
-            },
-            "POST"
-         );
-         console.log(
-            JSON.stringify({
-               credential: credentialResponse.credential,
-               clientId: credentialResponse.clientId,
-            })
-         );
-      }}
-      onError={() => {
-         console.log("Login Failed");
-      }}
-   />
-);
+const SignInGoogle = () => {
+   const [login, setLogin] = useRecoilState(loginState);
+   const setUserId = useSetRecoilState(userIdState);
+
+   return (
+      <GoogleLogin
+         nonce=""
+         onSuccess={(credentialResponse) => {
+            const response = generalFetch(
+               "auth/oauth",
+               JSON.stringify({
+                  credential: credentialResponse.credential,
+                  clientId: credentialResponse.clientId,
+               }),
+               {
+                  "Content-type": "application/json; charset=UTF-8",
+               },
+               "POST"
+            );
+            response.then((value) => {
+               if (typeof value === "number") {
+                  // do nothing
+               } else if (value) {
+                  setLogin(true);
+                  setUserId(value.id);
+                  setToken(value);
+                  return <Navigate to="/" />;
+               }
+            });
+         }}
+         onError={() => {
+            console.log("Login Failed");
+         }}
+      />
+   );
+};
 
 export default SignInGoogle;
