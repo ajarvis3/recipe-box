4af93a5ed8ef0ddf508a36e054ab700a82784e13
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2026-04-20 18:26:18 -0400

    Initial vite upgrade

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const domNode: Element = document.getElementById("root") as Element;
@@ -12,11 +11,5 @@ root.render(
       <React.StrictMode>
          <App />
       </React.StrictMode>
-   </GoogleOAuthProvider>
+   </GoogleOAuthProvider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// Breaks it? https://github.com/facebook/create-react-app/issues/10109#issuecomment-752421290
-reportWebVitals(console.log);
